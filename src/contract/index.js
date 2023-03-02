432fbf5a1e63131a55acd4993923037d28e89d57
Alex1114 <pokai.eed05@g2.nctu.edu.tw>
2023-03-02 11:15:34 +0800

    Update images

--- src/contract/index.js
+++ src/contract/index.js
@@ -5,5 +5,5 @@ export const own_function = "tokensOfOwner"
 export const contractChainId = "0x80001"
 export const max_mint = 1;
 export const total = 3000
-export const metamaskAppUrl = "https://metamask.app.link/dapp/wonderland.aicreate360.com"
+export const metamaskAppUrl = "https://metamask.app.link/dapp/wonderland.aicreate360.com/"
 export const openseaUrl = "https://testnets.opensea.io/zh-TW/collection/wonderland-aicreate360-2"
