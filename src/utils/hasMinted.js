7fed8d71ecbd17722a3902390f42ce2a75de37e1
mchu-1999 <jerry.ee06@nctu.edu.tw>
2023-03-04 17:15:16 +0800

    function update

--- src/utils/hasMinted.js
+++ src/utils/hasMinted.js
@@ -1,5 +1,5 @@
-import { contractChainId, nftPrice } from '../contract';
-import { ethers } from 'ethers';
+// import { contractChainId, nftPrice } from '../contract';
+// import { ethers } from 'ethers';
 import { contract_address } from '../contract';
 import Web3 from 'web3/dist/web3.min.js'
 
@@ -8,7 +8,8 @@ const contractABI = require('../contract/contract-abi.json');
 
 export const hasMinted = async (account) => {
   const currentContract = new web3.eth.Contract(contractABI, contract_address);
-  let quantity = await currentContract.methods.hasPSMinted(window.ethereum.selectedAddress).call();
-
+  // let address = await window.ethereum.request({ method: 'eth_accounts' });
+  // console.log(address);
+  let quantity = await currentContract.methods.hasPSMinted(account).call();
   return { quantity };
 };
